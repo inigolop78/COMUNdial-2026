7fc141a501e85b18bae0e87f98352f1f03afa75b
inigolop78 <inigolop@gmail.com>
2026-06-14 12:33:53 +0200

    Fix SW sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,7 @@
-// This service worker clears all caches and unregisters itself
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-     .then(() => self.registration.unregister())
+    caches.keys().then(k => Promise.all(k.map(c => caches.delete(c))))
+    .then(() => self.registration.unregister())
   );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
 });
