01889e61d5f82bf20e596e19bfe7ca3e6ec579d8
inigolop78 <inigolop@gmail.com>
2026-06-14 13:58:18 +0200

    No-cache headers v1781438296

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
-// v1781437008 - no cache, always fetch fresh
-self.addEventListener('install', e => { self.skipWaiting(); });
+// v1781438296
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-    .then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
+  // Always fetch from network, never cache
+  e.respondWith(fetch(e.request.url + (e.request.url.includes('?') ? '&' : '?') + '_=1781438296'));
 });
