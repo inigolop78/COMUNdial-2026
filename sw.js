e1026f204b5a27fbf56fe82b32359556a4c1166d
inigolop78 <inigolop@gmail.com>
2026-06-14 14:12:18 +0200

    Kill SW v1781439136

--- sw.js
+++ sw.js
@@ -1,13 +1,10 @@
-// v1781438296
+// v1781439136 - disabled
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
+    caches.keys().then(k => Promise.all(k.map(c => caches.delete(c))))
+    .then(() => self.registration.unregister())
+    .then(() => self.clients.claim())
   );
 });
-self.addEventListener('fetch', e => {
-  // Always fetch from network, never cache
-  e.respondWith(fetch(e.request.url + (e.request.url.includes('?') ? '&' : '?') + '_=1781438296'));
-});
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
