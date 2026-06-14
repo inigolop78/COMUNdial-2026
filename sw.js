a115461087e658a5d50cb5d6c79e9941954be30b
inigolop78 <inigolop@gmail.com>
2026-06-14 12:44:06 +0200

    Force update sw.js 1781433845

--- sw.js
+++ sw.js
@@ -1,7 +1,20 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => {
+const CACHE = 'mundial-1781433845';
+const FILES = ['./', './index.html', './style.css', './app.js', './data.js', './api.js', './manifest.json'];
+
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.keys().then(k => Promise.all(k.map(c => caches.delete(c))))
-    .then(() => self.registration.unregister())
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => caches.open(CACHE).then(c => c.addAll(FILES)))
+    .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
