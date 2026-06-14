015a91cc826ed2eee090f10110ad7c75462e26ac
inigolop78 <inigolop@gmail.com>
2026-06-14 17:33:11 +0200

    Kill SW and fix CSS v1781451190

--- sw.js
+++ sw.js
@@ -1,10 +1,19 @@
-// v1781439136 - disabled
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => {
+// KILL ALL CACHES
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.keys().then(k => Promise.all(k.map(c => caches.delete(c))))
-    .then(() => self.registration.unregister())
-    .then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.skipWaiting())
   );
 });
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.registration.unregister())
+    .then(() => self.clients.matchAll()).then(clients => {
+      clients.forEach(client => client.navigate(client.url));
+    })
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request, {cache: 'no-store'}));
+});
